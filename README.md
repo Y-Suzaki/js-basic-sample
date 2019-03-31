99b74e0ca3ed93e84fbe6ffef3d6d809c5e205f5
Y-Suzaki <geranium04.24.1981@gmail.com>
2019-03-31 19:00:00 +0900

    Add axios sample.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # js-basic-sample
+
+## development settings
+### npm
+* install node.js.
+* npm init, then package json is created.
+* install libraries with npm. Please refer to the following
+  ```
+  npm install browserifyg
+  npm install axios --save
+  ```
+* add require in source code.
+  ```
+  const axios = require('axios');
+  ```
+
+### IntelliJ
+* Preferences -> Languages & Frameworks -> Node.js and NPM  
+Coding Assistance -> Enableに変更
---
+++ js-sample-external-axios.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const url = "https://google.com";
+
+//
+// When axios get method is called, it returns the promise object.
+//
+axios.get(url)
+    .then(response => {
+        console.log(response.status);
+        //console.log(response.data);
+    })
+    .catch(e => {
+        console.log(e);
+    });
+
+//
+// improve the above code with async and wait grammar.
+//
+async function get(url) {
+    try {
+        const response = await axios.get(url);
+        console.log(response.status);
+    } catch (e) {
+        console.log(e)
+    }
+}
+
